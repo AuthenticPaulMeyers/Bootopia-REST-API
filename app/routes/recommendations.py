5d21c2ea8a9cf0927b7a6ea900c7afcf1a45f06d
paul meyers <authenticpaulmeyers@gmail.com>
2025-06-05 18:18:47 +0200

    Edited recommendations route

--- app/routes/recommendations.py
+++ app/routes/recommendations.py
@@ -4,15 +4,22 @@ from ..services.get_recommendations import get_mood_recommendations
 
 recommender = Blueprint('recommendations', __name__, url_prefix='/recommendations')
 
-@recommender.route('/')
+@recommender.route('/mood')
 def mood_based_recommendations():
     mood = request.args.get('mood')
 
     if mood == "":
-        return {"message": "Mood should not be empty."}, HTTP_400_BAD_REQUEST
+        return {"error": "Mood should not be empty."}, HTTP_400_BAD_REQUEST
 
     book_recommendations = get_mood_recommendations(mood)
 
     return {
         'recommendations': book_recommendations
     }, HTTP_200_OK
+
+@recommender.route('/')
+def user_based_recommendations():
+    user_id = request.args.get('user_id')
+
+    if user_id == "":
+        return {"error": "User not found."}, HTTP_400_BAD_REQUEST
