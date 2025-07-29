8184cc54a6d331c3641877f81c4b775539f7ac7e
paul meyers <pmkundika@gmail.com>
2025-07-29 21:27:33 +0200

    Cleaned the code with added comments

--- app/auths/user_auth.py
+++ app/auths/user_auth.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from ..schema.models import db, Users
 from flask_jwt_extended import jwt_required, create_refresh_token, get_jwt_identity, create_access_token
 from ..utils.image_upload import upload_image
-from ..constants.http_status_codes import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR
+from ..constants.http_status_codes import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR, HTTP_201_CREATED
 import validators
 from ..utils.send_email import send_password_reset_email
 from app import limiter, get_remote_address
@@ -14,7 +14,7 @@ auth = Blueprint('auth', __name__, static_url_path='static/', url_prefix='/api/v
 # register route
 @auth.route('/register', methods=['POST', 'GET'])
 def register():
-    # get user details
+    # get user details and create variables
     if request.method == 'POST':
         username = request.form.get('username')
         email = request.form.get('email')
@@ -26,35 +26,37 @@ def register():
         if password != confirm_password:
             return jsonify({'error': 'Passwords do not match.'}), HTTP_400_BAD_REQUEST
         
+        # Validate password length
         if len(password) < 8:
             return jsonify({'error': 'Password must be at least 8 characters long.'}), HTTP_400_BAD_REQUEST
         
-        # validating the name 
+        # validate the username 
         if len(username) < 3:
-            return jsonify({'error': "Name is too short"}), HTTP_400_BAD_REQUEST
+            return jsonify({'error': "Username is too short."}), HTTP_400_BAD_REQUEST
         
         if not username.isalnum() or " " in username:
-            return jsonify({"error": "Name should not contain numbers or symbols"}), HTTP_400_BAD_REQUEST
+            return jsonify({"error": "Username should not contain numbers or symbols."}), HTTP_400_BAD_REQUEST
         
         if password == '' or not password or not username or not email or not bio or bio == '':
-            return jsonify({'error': 'Required fields should not be empty.'})
+            return jsonify({'error': 'Required fields should not be empty.'}), HTTP_400_BAD_REQUEST
         
         # validate the user email
         if not validators.email(email):
-            return jsonify({"error": "Email is not valid"})
+            return jsonify({"error": "Email is not valid"}), HTTP_400_BAD_REQUEST
         
         # check if the user email is not already registered in the database
         if Users.query.filter_by(email=email).first():
-            return jsonify({'error': "Email already exist"}), HTTP_409_CONFLICT
+            return jsonify({'error': "Email already exist."}), HTTP_409_CONFLICT
         
         # check if the username is not already registered in the database
         if Users.query.filter_by(username=username).first():
-            return jsonify({"error": 'Username already exist'}), HTTP_409_CONFLICT
+            return jsonify({"error": 'Username already exist.'}), HTTP_409_CONFLICT
+        
+        # generate hashed password
+        password_hashed = generate_password_hash(password)
 
         # File upload
         file = request.files.get('file')
-        # generate hashed password
-        password_hashed = generate_password_hash(password)
 
         if not file:
             return jsonify({'error': 'No file provided.'}), HTTP_400_BAD_REQUEST
@@ -63,6 +65,7 @@ def register():
         if not file_url:
             return jsonify({'error': 'Invalid file type.'}), HTTP_400_BAD_REQUEST
 
+        # Add user to the database
         user = Users(username=username, email=email, password_hash=password_hashed, bio=bio, profile_pic_url=file_url)
         db.session.add(user)
         db.session.commit()
@@ -75,12 +78,13 @@ def register():
                 'bio': bio,
                 'profile_pic_url': file_url
             }
-        }), 201
+        }), HTTP_201_CREATED
 
 # login route
 @auth.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
+        # get variables
         email = request.json.get("email")
         password = request.json.get("password")
 
@@ -105,7 +109,7 @@ def login():
                         'id': user.id
                     }
                 }), HTTP_200_OK
-        return jsonify({'error': 'wrong username or password'}), HTTP_400_BAD_REQUEST
+        return jsonify({'error': 'Wrong username or password.'}), HTTP_400_BAD_REQUEST
 
 # get current user profile route
 @auth.route('/me', methods=['POST', 'GET'])
